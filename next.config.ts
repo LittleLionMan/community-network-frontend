c29d03911df47fb866bab5eadd99afec961eb42e
N S <sibnie@web.de>
2025-11-01 01:33:17 +0100

    next-config ohne standalone

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-
   async headers() {
     return [
       {
